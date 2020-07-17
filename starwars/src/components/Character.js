3a5603f8fb9617c3e683903ea5b2613273f42ff0
Jacevedo21 <jacevedo0213@gmail.com>
2020-07-17 13:25:54 -0400

    Added buttons

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,10 +1,10 @@
 // Write your Character component here
 import React from 'react'
-
+import { Container } from '../components/styles'
 
 export default function Character(props) {
     console.log(props)
-        const {
+    const {
         gender,
         name,
         image,
@@ -13,17 +13,25 @@ export default function Character(props) {
         status,
         species,
     } = props.character;
-    // console.log(props.character)
-    console.log(gender)
+    console.log(location)
+    // console.log(gender)
+
+
     return (
-        <div>
+        <Container direction='column' className='card'>
             <h3>{props.character.name}</h3>
-            <img src={props.character.image} alt={name}/>
-            <p>Gender: {props.character.gender}</p>
-            <p>Species: {props.character.species}</p>
-            {/* <p>Location: {props.character.location}</p> */}
-            {/* <p>Origin: {origin}</p> */}
-            {/* <p>Status: {status}</p> */}
-        </div>
+            <Container>
+                <Container width={'40%'}>
+                    <img src={props.character.image} alt={name} />
+                </Container>
+                <Container width ={'40%'} direction={'column'}>
+                <p>Gender: {props.character.gender}</p>
+                <p>Species: {props.character.species}</p>
+                <p>Location: {props.character.location.name}</p>
+                <p>Origin: {props.character.origin.name}</p>
+                <p>Status: {status}</p>
+                </Container>
+            </Container>
+        </Container>
     )
 }
